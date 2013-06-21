b2eaea382d969554a63074cd6c3be9b9e59633f3
Christopher Broome <ChrisBroome7@gmail.com>
2013-06-20 22:06:43 -0400

    Add basic static file serving

--- app.js
+++ app.js
@@ -3,44 +3,66 @@
 
   var http = require('http');
   var util = require('util');
+  var fs = require('fs');
+  var path = require('path');
 
   var contentTypes = {
+    'text': 'text/plain',
     'html': 'text/html',
-    'json': 'application/json',
-    'text': 'text/plain'
+    'css': 'text/css',
+    'js': 'text/javascript',
+    'json': 'application/json'
   };
 
   var router = {
     '/': function(request, response) {
-      setContentType('html', response);
+      setContentType(request, response, 'html');
       response.write('<h1>Hi!</h1>');
     }
   };
-
 
   var server = http.createServer(requestListener);
 
   server.listen(3333);
 
   function requestListener(request, response) {
-    var handler = router[request.url];
+    var action = router[request.url];
     logger(request);
-    if( handler ) {
-      handler(request, response);
+    if( action ) {
+      action(request, response);
+      response.end();
     }
     else {
-      response.statusCode = 404;
-      response.write('Error: Not Found');
+      staticFileProcessor(request, response);
     }
-    response.end();
   }
 
-  function setContentType(type, response) {
+  function setContentType(request, response, type) {
     response.setHeader('Content-Type', contentTypes[type] || contentTypes.text);
   }
 
   function logger(request) {
     util.log(request.method + ' ' + request.url);
+  }
+
+  function notFound(response) {
+    response.statusCode = 404;
+    response.write('Error: Not Found');
+  }
+
+  function staticFileProcessor(request, response) {
+    var fsPath = path.normalize(path.join(__dirname, request.url));
+    fs.exists(fsPath, function(exists) {
+      if( exists ) {
+        var ext = path.extname(fsPath).substr(1);
+        setContentType(request, response, ext)
+        var readStream = fs.createReadStream(fsPath);
+        readStream.pipe(response);
+      } else {
+        notFound(response);
+        response.end();
+      }
+    });
   }
 
 }).call(this);
