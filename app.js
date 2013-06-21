1427e7bf7f6926726c6206c31ad397aa754412fe
Christopher Broome <ChrisBroome7@gmail.com>
2013-06-20 21:05:17 -0400

    Add better logging, proper 404 response

--- app.js
+++ app.js
@@ -17,27 +17,30 @@
     }
   };
 
-  function setContentType(type, response) {
-    response.setHeader('Content-Type', contentTypes[type] || contentTypes.text);
-  }
 
-  function logger(request) {
-    console.log(util.inspect(request, {colors: true}));
-  }
+  var server = http.createServer(requestListener);
 
-  var server = http.createServer(function requestListener(request, response) {
+  server.listen(3333);
+
+  function requestListener(request, response) {
     var handler = router[request.url];
     logger(request);
     if( handler ) {
       handler(request, response);
     }
     else {
-      response.write('Error: Not Found');
       response.statusCode = 404;
+      response.write('Error: Not Found');
     }
     response.end();
-  });
+  }
 
-  server.listen(3333);
+  function setContentType(type, response) {
+    response.setHeader('Content-Type', contentTypes[type] || contentTypes.text);
+  }
+
+  function logger(request) {
+    util.log(request.method + ' ' + request.url);
+  }
 
 }).call(this);
