3b5d94e2e9382c2c84be86a2f77d424a841fc637
Christopher Broome <ChrisBroome7@gmail.com>
2013-06-21 22:30:33 -0400

    Add sha1file.js
    Calculates the sha1sum of a file given by the first argument of the
    command line.

---
+++ sha1file.js
@@ -0,0 +1,35 @@
+(function() {
+  var context = this;
+
+  var crypto = require('crypto');
+  var fs = require('fs');
+  var util = require('util');
+  var stream = require('stream');
+  var Transform = stream.Transform;
+
+  var filename = process.argv[2];
+  var shasum = crypto.createHash('sha1');
+  var readStream = fs.createReadStream(filename);
+
+  var hexTransform = new Transform({objectMode: true});
+  hexTransform._transform = function(chunk, encoding, done) {
+    var data = chunk.toString('hex');
+    this.push(data);
+    done();
+  }
+  var newLineTransform = new Transform({objectMode: true});
+  newLineTransform._transform = stream.PassThrough.prototype._transform;
+  newLineTransform._flush = function() {
+    this.push('\n');
+  }
+
+  var streams = [readStream, shasum, hexTransform, newLineTransform, process.stdout];
+
+  // like calling streams[0].pipe(streams[1]). ... .pipe(streams[n-1])
+  streams.reduce(pipeIt);
+
+  function pipeIt(a, x) {
+    return a.pipe(x);
+  }
+
+}).call(this);
