ffbd18015168f441c48c0e30a6c88ebd90573a99
Christopher Broome <ChrisBroome7@gmail.com>
2013-06-21 23:18:56 -0400

    HexTransform: add eol option

--- transforms/HexTransform.js
+++ transforms/HexTransform.js
@@ -1,11 +1,13 @@
 (function() {
   var context = this;
 
+  var os = require('os');
   var stream = require('stream');
   var PassThrough = stream.PassThrough;
 
   function HexTransform(options) {
     PassThrough.call(this, options);
+    this._eol = options.eol;
   }
   HexTransform.prototype = Object.create( PassThrough.prototype, {
     constructor: {value: HexTransform}
@@ -14,6 +16,11 @@
     var data = chunk.toString('hex');
     this.push(data);
     done();
+  }
+  HexTransform.prototype._flush = function() {
+    if( this._eol ) {
+      this.push(os.EOL);
+    }
   }
   module.exports = HexTransform;
 
