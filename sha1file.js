ce10a2f44189b924fd2ebbe9337f6380679ae238
Christopher Broome <ChrisBroome7@gmail.com>
2013-06-21 23:19:30 -0400

    sha1file: add processing for multiple files

--- sha1file.js
+++ sha1file.js
@@ -3,25 +3,25 @@
 
   var crypto = require('crypto');
   var fs = require('fs');
-  var util = require('util');
-  var stream = require('stream');
-  var Transform = stream.Transform;
   var HexTransform = require('./transforms/HexTransform');
 
-  var filename = process.argv[2];
-  var shasum = crypto.createHash('sha1');
-  var readStream = fs.createReadStream(filename);
+  var eol = process.argv.indexOf('--eol') >= 0;
 
-  var hexTransform = new HexTransform({objectMode: true});
-  var newLineTransform = new stream.PassThrough({objectMode: true});
-  newLineTransform._flush = function() {
-    this.push('\n');
+  var files = process.argv.slice(2).filter(function(arg) {
+    return arg.indexOf('--') < 0;
+  });
+
+  // compute the sha1sum for each input file
+  files.forEach(sha1sum);
+
+  function sha1sum(filename) {
+    var shasum = crypto.createHash('sha1');
+    var readStream = fs.createReadStream(filename);
+    var hexTransform = new HexTransform({objectMode: true, eol: eol});
+    var streams = [readStream, shasum, hexTransform, process.stdout];
+    // like calling streams[0].pipe(streams[1]). ... .pipe(streams[n-1])
+    streams.reduce(pipeIt);
   }
-
-  var streams = [readStream, shasum, hexTransform, newLineTransform, process.stdout];
-
-  // like calling streams[0].pipe(streams[1]). ... .pipe(streams[n-1])
-  streams.reduce(pipeIt);
 
   function pipeIt(a, x) {
     return a.pipe(x);
